ba85cbccfe443d9a9d0fd7baf4bb4897f2746f24
Youngwook-Go <Youngwook-Go@users.noreply.github.com>
2022-10-18 17:57:24 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -10,16 +10,21 @@
 
 function submit() {
   // input
-  const numberOfHours = parseFloat(document.getElementById("worked-hours").value)
-  const hourlyRate = parseFloat(document.getElementById('hourly-rate').value)
+  const numberOfHours = parseFloat(
+    document.getElementById("worked-hours").value
+  )
+  const hourlyRate = parseFloat(document.getElementById("hourly-rate").value)
   const TAX_RATE = 0.18
 
   // process
-  const governmentTake = (numberOfHours * hourlyRate) * TAX_RATE
-  const homePay = (numberOfHours * hourlyRate) - governmentTake
+  const governmentTake = numberOfHours * hourlyRate * TAX_RATE
+  const homePay = numberOfHours * hourlyRate - governmentTake
 
   // output
-  document.getElementById('homePay').innerHTML = `Your pay will be: $ ${homePay.toFixed(2)}`
-  document.getElementById('governmentTake').innerHTML = `Your government took: $ ${governmentTake.toFixed(2)}`
+  document.getElementById(
+    "homePay"
+  ).innerHTML = `Your pay will be: $ ${homePay.toFixed(2)}`
+  document.getElementById(
+    "governmentTake"
+  ).innerHTML = `Your government took: $ ${governmentTake.toFixed(2)}`
 }
-
